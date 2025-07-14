12409e197870dca7fa019764e755a84e390a6a01
Jeremy Marube <jeremymarube1@gmail.com>
2025-07-14 07:43:58 +0300

    Adding content

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      const timeoutId = setTimeout(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+
+      return () => clearTimeout(timeoutId);
+    } else {
+      onAnswered(false);
+      setTimeRemaining(10);
+    }
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -30,3 +41,4 @@ function Question({ question, onAnswered }) {
 }
 
 export default Question;
+
